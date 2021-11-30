bc732038e06c93176cb2791617db896bb639d3dd
anduen0 <anduenbeqirii@gmail.com>
2021-11-30 17:31:16 +0100

    Soluzione esercizio lezione 4

---
+++ CSVfile.py
@@ -0,0 +1,20 @@
+class CSVfile:
+	def __init__(self, name):
+		self.name = name
+	
+	def get_data(self):
+
+		my_file = open(self.name, 'r')
+
+		data=[]
+
+		for line in my_file:
+			
+			if 'Date' in line:
+				continue
+
+			data.append(line.split(','))
+		
+		my_file.close()
+
+		return data
---
+++ main.py
@@ -0,0 +1,5 @@
+from CSVfile import CSVfile
+
+my_csv_file = CSVfile('./shampoo_sales.csv')
+print(my_csv_file.get_data())
+
